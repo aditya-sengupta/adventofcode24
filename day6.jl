51a25c5fb86421573819ea76ded3752e13cd07de
aditya-sengupta <adityars@ucsc.edu>
2024-12-06 14:46:27 -0800

    6-2 new attempt still wrong

--- day6.jl
+++ day6.jl
@@ -2,71 +2,67 @@ using DelimitedFiles
 using ProgressMeter
 using Base.Threads: @threads
 
-maze_orig = permutedims(hcat(split.(readdlm("inputs/day6_test.txt"), "")...), (2, 1))
+maze = permutedims(hcat(split.(readdlm("inputs/day6.txt"), "")...), (2, 1))
 next_move = Dict("^" => ">", ">" => "v", "v" => "<", "<" => "^")
 starting_position = (x -> [x[1], x[2]])(only(filter(x -> length(x) > 0, union(
-        findall(maze_orig .== x)
+        findall(maze .== x)
         for x in ["^", ">", "<", "v"]
     )))[1])
-starting_orientation = maze_orig[starting_position...]
+starting_orientation = maze[starting_position...]
+maze[starting_position...] = "."
 
-function proposed_move(current_position, move_direction)
-    proposed_position = copy(current_position)
+function proposal(state)
+    next = copy(state)
+    move_direction = state[3]
     if move_direction == "^"
-        proposed_position[1] -= 1
+        next[1] -= 1
     elseif move_direction == ">"
-        proposed_position[2] += 1
+        next[2] += 1
     elseif move_direction == "<"
-        proposed_position[2] -= 1
+        next[2] -= 1
     elseif move_direction == "v"
-        proposed_position[1] += 1
+        next[1] += 1
     end
-    return proposed_position
+    return next
 end
 
-function maze_walk(maze, starting_position, starting_orientation)
-    walked_maze = copy(maze_orig)
-    current_position = copy(starting_position)
-    walk_locations = []
-    move_direction = starting_orientation
-    move_directions = []
+function advance!(state, extra_obstacle=[0,0])
     while true
-        proposed_position = current_position
-        while !(walked_maze[proposed_position[1], proposed_position[2]] in [".", "X"])
-            proposed_position = proposed_move(current_position, move_direction)
-            if !(1 <= proposed_position[1] <= size(maze, 1)) || !(1 <= proposed_position[2] <= size(maze, 2))
-                return walk_locations, move_directions, 0
-            elseif maze[proposed_position[1], proposed_position[2]] == "#"
-                move_direction = next_move[move_direction]
-            end
+        proposed_state = proposal(state)
+        if off_grid(proposed_state) || (maze[proposed_state[1], proposed_state[2]] != "#" && !all(proposed_state[1:2] .== extra_obstacle))
+            state[:] = proposed_state
+            return
         end
-        check_infinite_loop = findall([all(x .== current_position) for x in walk_locations])
-        @show current_position, proposed_position
-        @show check_infinite_loop
-        if length(check_infinite_loop) > 0
-            if move_direction in move_directions[check_infinite_loop]
-                return walk_locations, move_directions, 1
-            end
-        end
-        walked_maze[current_position[1], current_position[2]] = "X"
-        push!(walk_locations, current_position)
-        push!(move_directions, move_direction)
-        current_position = proposed_position
-        walked_maze[current_position[1], current_position[2]] = move_direction
+        state[3] = next_move[state[3]]
     end
 end
 
-maze_walk(maze_orig, [6, 5], "^")
-
-function day6_1(maze_orig, starting_position, starting_orientation)
-     return 1 + length(Set(maze_walk(maze_orig, starting_position, starting_orientation)[1]))
+function off_grid(state)
+    return !(1 <= state[1] <= size(maze, 1)) || !(1 <= state[2] <= size(maze, 2))
 end
 
-day6_1(maze_orig, starting_position, starting_orientation)
+seen_states = Tuple[]
+current_state = [starting_position[1], starting_position[2], starting_orientation]
+while !off_grid(current_state) && !(Tuple(current_state) in seen_states)
+    push!(seen_states, Tuple(current_state))
+    advance!(current_state)
+end
+seen_states
+length(Set([[x[1], x[2]] for x in seen_states])) # part 1
 
-walk_locations, move_directions, infinite_loops = maze_walk(maze_orig, starting_position, starting_orientation)
-
-for (w, d) in zip(walk_locations, move_directions)
-    println(w, d)
-    @assert maze_walk(maze_orig, w, d)[3] == 0
+n_infinite_loops = 0
+@showprogress @threads for i in 1:(length(seen_states)-1)
+    # start iteration from each point along the unobstructed path
+    # place an obstacle at the one just after
+    state = seen_states[i]
+    extra_obstacle_state = seen_states[i+1]
+    current_state = [state[1], state[2], state[3]]
+    seen_states_new = Tuple[]
+    while !off_grid(current_state) && !(Tuple(current_state) in seen_states_new)
+        push!(seen_states_new, Tuple(current_state))
+        advance!(current_state, [extra_obstacle_state[1], extra_obstacle_state[2]])
+    end
+    if !(off_grid(current_state))
+        global n_infinite_loops += 1
+    end
 end
