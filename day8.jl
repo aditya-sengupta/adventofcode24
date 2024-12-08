f5c6cd26c69ebbb0131a80d7622358d11d479072
aditya-sengupta <adityars@ucsc.edu>
2024-12-08 12:20:47 -0800

    day 8 optimized a bit

--- day8.jl
+++ day8.jl
@@ -1,49 +1,37 @@
-grid = hcat(split.(readlines("inputs/day8.txt"), "")...)
-n = size(grid, 1)
-
-node_types = filter(x -> x != ".", unique(grid))
+raw_input = readlines("inputs/day8.txt")
 
 function in_bounds(x)
     (1 <= x[1] <= n) && (1 <= x[2] <= n)
 end
 
-function day8_1()
-    antinode_locations = Set()
-    for n in node_types
-        grid_locations = findall(grid .== n)
-        for idx1 in eachindex(grid_locations)
-            for idx2 in idx1+1:length(grid_locations)
-                push!(antinode_locations, 2 * grid_locations[idx1] - grid_locations[idx2])
-                push!(antinode_locations, 2 * grid_locations[idx2] - grid_locations[idx1])
-            end
-        end
-    end
-    filter!(in_bounds, antinode_locations)
-    length(antinode_locations)
-end
+function day8()
+    grid = hcat(split.(raw_input, "")...)
+    n = size(grid, 1)
 
-function day8_2()
-    antinode_locations = Set()
+    node_types = filter(x -> x != ".", unique(grid))
+    antinode_locations_one = Set()
+    antinode_locations_two = Set(findall(x -> x .!= ".", grid))
     for n in node_types
         grid_locations = findall(grid .== n)
         for idx1 in eachindex(grid_locations)
             for idx2 in idx1+1:length(grid_locations)
                 diff_vector = grid_locations[idx1] - grid_locations[idx2]
-                curr_location = grid_locations[idx1]
+                curr_location = 2 * grid_locations[idx1] - grid_locations[idx2]
+                if in_bounds(curr_location) push!(antinode_locations_one, curr_location) end
                 while in_bounds(curr_location)
-                    push!(antinode_locations, curr_location)
+                    push!(antinode_locations_two, curr_location)
                     curr_location += diff_vector
                 end
-                curr_location = grid_locations[idx2]
+                curr_location = 2 * grid_locations[idx2] - grid_locations[idx1]
+                if in_bounds(curr_location) push!(antinode_locations_one, curr_location) end
                 while in_bounds(curr_location)
-                    push!(antinode_locations, curr_location)
+                    push!(antinode_locations_two, curr_location)
                     curr_location -= diff_vector
                 end
             end
         end
     end
-    length(antinode_locations)
+    return length(antinode_locations_one), length(antinode_locations_two)
 end
 
-@time day8_1()
-@time day8_2()
+@time day8()
